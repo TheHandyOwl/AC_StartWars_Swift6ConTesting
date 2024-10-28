c94e317f858616d39de7307e5ee4c6fce957253b
Carlos Company <carlos.company@gigigo.com>
2024-10-28 23:03:50 +0100

    Initial Commit

---
+++ AC_StartWars_Swift6ConTesting/AC_StartWars_Swift6ConTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AC_StartWars_Swift6ConTestingApp.swift
+//  AC_StartWars_Swift6ConTesting
+//
+//  Created by Carlos Company on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AC_StartWars_Swift6ConTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
